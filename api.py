5d7443b0a3319d4223d45da4f678fa5b34d731d9
Eason Liu <bettercalleason@gmail.com>
2022-11-19 04:26:13 +0800

    1118

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ import pymysql
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
-CORS(app, resources={r"./*":{"origins":["http://127.0.0.1:5500"]}})
+CORS(app, resources={r"./*":{"origins":["*"]}})
 
 db = pymysql.connect(
     host='localhost',
@@ -36,11 +36,49 @@ def home():
 def menu():
     return jsonify(menuData)
 
-@app.route('/school',methods=['GET'])
-def scoolGetAll():
-    res = {"success":False, "info":"查詢失敗"}
+
+''' CRUD 開始 '''
+
+# 新增學員 API
+@app.route('/newStudent',methods=['POST'])
+def newStudent():
+    res = {"success":False, "info":"註冊失敗"}
     try:
-        sql = 'SELECT * FROM `students` WHERE `s_id` < 5'
+        sql = 'INSERT INTO `students` (`s_name`,`s_gender`,`s_nickname`) VALUES (%s,%s,%s)'
+        cursor.execute(sql, (request.
+        json['s_name'], request.json['s_gender'],request.json['s_nickname']))
+
+        if cursor.rowcount > 0:
+
+            res['success'] = True
+            res['info'] = '新增成功'
+
+        else:
+            res['info'] = '新增失敗'
+
+        db.commit()
+
+    except Exception as e:
+        db.rollback()
+        res['info']= f'SQL 執行失敗: {e}'
+
+    return jsonify(res)
+
+
+# 讀取 API
+@app.route('/school/read',methods=['GET'])
+def scoolRead():
+    res = {"success":False, "info":"查詢失敗"}
+    whereSqlString = ' WHERE ';
+
+    for key, value in list(request.args.lists()):
+        if value[0] :
+            whereSqlString += f'`{key}`="{value[0]}" OR '
+
+    finalSQL = whereSqlString[:-3]
+
+    try:
+        sql = f'SELECT * FROM `students` {finalSQL}'
         cursor.execute(sql)
 
         if cursor.rowcount > 0:
@@ -60,20 +98,78 @@ def scoolGetAll():
 
     return jsonify(res)
 
-@app.route('/school/<int:id>',methods=['DELETE'])
-def delStudent(id):
-    res = {"success":False, "info":"刪除失敗"}
+
+# 更新學員資料 API
+@app.route('/school/update/<int:id>',methods=['PUT'])
+def update(id):
+    res = {"success":False, "info":"修改失敗"}
+    print(id)
+    setSqlString = ' SET ';
+    print(request.json)
+
+    for key, value in list(request.json.items()):
+        if key != 's_id' and value :
+            setSqlString += f'`{key}`="{value}" ,'
+
+    finalSQL = setSqlString[:-1]
+    print(finalSQL)
+
     try:
-        sql = 'DELETE FROM `students` WHERE `s_id` = %s'
-        cursor.execute(sql, (id))
+        sql = f'UPDATE `students` {finalSQL} WHERE `s_id`={id}'
+        print('sql',sql)
+        cursor.execute(sql)
 
         if cursor.rowcount > 0:
 
+            res['success'] = True
+            res['info'] = '修改成功'
+
+        else:
+            res['info'] = '修改失敗'
+
+        db.commit()
+
+    except Exception as e:
+        db.rollback()
+        res['info']= f'SQL 執行失敗: {e}'
+
+    return jsonify(res)
+
+# 刪除學員資料 API
+@app.route('/school/delete/<int:id>',methods=['DELETE'])
+def delete(id):
+    res = {"success":False, "info":"修改失敗"}
+    setSqlString = ''
+    print(request.json)
+
+    if (id):
+        setSqlString += f' WHERE `s_id`={id}'
+        if len(list(request.json.items())) > 0:
+            setSqlString += ' OR '
+
+    if len(list(request.json.items())) > 0 and id == False:
+        setSqlString += ' WHERE '
+
+    for key, value in list(request.json.items()):
+        if key != 's_id' and value :
+            setSqlString += f'`{key}`="{value}" OR '
+    
+
+    finalSQL = setSqlString[:-3]
+    print(finalSQL)
+
+    try:
+        sql = f'DELETE FROM `students` {finalSQL}'
+        print('sql',sql)
+        cursor.execute(sql)
+
+        if cursor.rowcount > 0:
+            result = cursor.fetchall()
             res['success'] = True
             res['info'] = '刪除成功'
-
+            res['result'] = result
         else:
-            res['info'] = '查無資料'
+            res['info'] = '無人可刪除'
 
         db.commit()
 
@@ -84,31 +180,7 @@ def delStudent(id):
     return jsonify(res)
 
 
-
-@app.route('/newStudent',methods=['POST'])
-def newStudent():
-    res = {"success":False, "info":"註冊失敗"}
-    try:
-        sql = 'INSERT INTO `students` (`s_name`,`s_gender`,`s_nickname`) VALUES (%s,%s,%s)'
-        cursor.execute(sql, (request.json['name'], request.json['gender'],request.json['nickname']))
-
-        if cursor.rowcount > 0:
-
-            res['success'] = True
-            res['info'] = '新增成功'
-
-        else:
-            res['info'] = '新增失敗'
-
-        db.commit()
-
-    except Exception as e:
-        db.rollback()
-        res['info']= f'SQL 執行失敗: {e}'
-
-    return jsonify(res)
-
-
+# 登入簡易示範
 @app.route('/login',methods=['POST'])
 def login():
     res = {"success":False, "info":"登入失敗"}
@@ -136,29 +208,6 @@ def login():
 
     return jsonify(res)
 
-@app.route('/update/<int:id>',methods=['PUT'])
-def update(id):
-    res = {"success":False, "info":"修改失敗"}
-
-    try:
-        sql = "UPDATE FROM `students` SET `s_name` = %s ,`s_nickname`= %s WHERE `s_id` = %s"
-        cursor.execute(sql, (request.json['name'],request.json['nickname'],id))
-
-        if cursor.rowcount > 0:
-
-            res['success'] = True
-            res['info'] = '修改成功'
-
-        else:
-            res['info'] = '修改失敗'
-
-        db.commit()
-
-    except Exception as e:
-        db.rollback()
-        res['info']= f'SQL 執行失敗: {e}'
-
-    return jsonify(res)
 
 
 app.run()
